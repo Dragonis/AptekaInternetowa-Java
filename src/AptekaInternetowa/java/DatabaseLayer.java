302366b272277d5e328b3595864d46360dde1354
Dragonis <wojtek.sasiela@gmail.com>
2015-01-13 20:42:46 +0100

    odznaczenie wszystkich lekow

--- src/AptekaInternetowa/java/DatabaseLayer.java
+++ src/AptekaInternetowa/java/DatabaseLayer.java
@@ -17,9 +17,9 @@ public class DatabaseLayer {
 
     public ArrayList<Lek> getNoweLeki() {
 
-        Lek nowe1 = new Lek(1, "Lek1", "Producent", 50, 30, true);
+        Lek nowe1 = new Lek(1, "Lek1", "Producent", 50, 30, false);
         Lek nowe2 = new Lek(2, "lek2", "Producent2", 60, 80, false);
-        Lek nowe3 = new Lek(3, "lek3", "Producent3", 70, 40, true);
+        Lek nowe3 = new Lek(3, "lek3", "Producent3", 70, 40, false);
 
         set.add(nowe1); set.add(nowe2); set.add(nowe3);
         
@@ -41,9 +41,9 @@ public class DatabaseLayer {
     public ArrayList<Lek> getLekidoZaplaty()
     {
         /*
-        Lek doZaplaty1 = new Lek(1, "AAA", "ProducenteA", 50, 30, Boolean.TRUE);
+        Lek doZaplaty1 = new Lek(1, "AAA", "ProducenteA", 50, 30, Boolean.FALSE);
         Lek doZaplaty2 = new Lek(2, "BBB", "ProducentB", 60, 80, Boolean.FALSE);
-        Lek doZaplaty3 = new Lek(3, "CCC", "ProducentC", 70, 40, Boolean.TRUE);
+        Lek doZaplaty3 = new Lek(3, "CCC", "ProducentC", 70, 40, Boolean.FALSE);
         
         set.add(doZaplaty1); set.add(doZaplaty2); set.add(doZaplaty3);
         */
